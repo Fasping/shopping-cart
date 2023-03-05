53249c4b657fe1013bce06125821c9f0aa1b0b5e
Fernando Cases <nandocasesgarcia@gmail.com>
2023-03-05 21:30:40 +0100

    fix

--- shopping-cart-react/src/App.jsx
+++ shopping-cart-react/src/App.jsx
@@ -1,5 +1,12 @@
+import { Products } from "./components/Products";
+import { products } from "./mock/products.json";
+
 function App() {
-  return <h1>Shopping Cart 🛒</h1>;
+  return;
+  <>
+    <h1>Shopping Cart 🛒</h1>
+    <Products products={products} />
+  </>;
 }
 
 export default App;
