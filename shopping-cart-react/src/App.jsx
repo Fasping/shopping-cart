5d33a2fc304663e87266d578117e09c8f27d0ecc
Fernando Cases <nandocasesgarcia@gmail.com>
2023-03-06 15:59:21 +0100

    fix: error mapping products

--- shopping-cart-react/src/App.jsx
+++ shopping-cart-react/src/App.jsx
@@ -2,11 +2,7 @@ import { Products } from "./components/Products";
 import { products } from "./mock/products.json";
 
 function App() {
-  return;
-  <>
-    <h1>Shopping Cart 🛒</h1>
-    <Products products={products} />
-  </>;
+  return <Products products={products} />;
 }
 
 export default App;
--- shopping-cart-react/src/components/Products.jsx
+++ shopping-cart-react/src/components/Products.jsx
@@ -1,34 +1,23 @@
 import "./Products.css";
-import { AddToCartIcon, RemoveFromCartIcon } from "./Icons.jsx";
+import { AddToCartIcon } from "./Icons";
 
 export function Products({ products }) {
   return (
     <main className="products">
       <ul>
-        {products.slice(0, 10).map((product) => {
-          const isProductInCart = checkProductInCart(product);
-
-          return (
-            <li key={product.id}>
-              <img src={product.thumbnail} alt={product.title} />
-              <div>
-                <strong>{product.title}</strong> - ${product.price}
-              </div>
-              <div>
-                <button
-                  style={{ backgroundColor: isProductInCart ? "red" : "#09f" }}
-                  onClick={() => {
-                    isProductInCart
-                      ? removeFromCart(product)
-                      : addToCart(product);
-                  }}
-                >
-                  {isProductInCart ? <RemoveFromCartIcon /> : <AddToCartIcon />}
-                </button>
-              </div>
-            </li>
-          );
-        })}
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.thumbnail} alt={product.title} />
+            <div>
+              <strong>{product.title}</strong>
+            </div>
+            <div>
+              <button>
+                <AddToCartIcon />
+              </button>
+            </div>
+          </li>
+        ))}
       </ul>
     </main>
   );
