a082b852907299ccb48036c159324c36b1961abd
Fernando Cases <nandocasesgarcia@gmail.com>
2023-03-06 16:21:56 +0100

    fix: type initial state filters

--- shopping-cart-react/src/App.jsx
+++ shopping-cart-react/src/App.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 function App() {
   const [products] = useState(initialProducts);
 
-  const [filter, setFilter] = useState({
+  const [filters, setFilter] = useState({
     category: "all",
     minPrice: 0,
   });
